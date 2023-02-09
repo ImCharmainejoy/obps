1c25bf0af5185ee118c10914cd3a7c1e99b84992
PatCode9 <45809622+PatCode9@users.noreply.github.com>
2023-02-09 12:59:34 +0800

    change api backend address

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,8 +56,8 @@ module.exports = {
     '~/assets/style/helvetica-neue/helvetica-neue.css'
   ],
   env: {
-    API_URL: 'http://202.137.120.41:50506/api', // waiting for the api xd
-    VUE_API_URL: 'http://202.137.120.41:50506/' // waiting for the file api xd
+    API_URL: 'https://ocbostaging.poolreno.com/api', // waiting for the api xd
+    VUE_API_URL: 'https://ocbostaging.poolreno.com/' // waiting for the file api xd
   },
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
