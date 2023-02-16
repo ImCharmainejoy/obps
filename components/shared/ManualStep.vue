6d24b20afc5b546b557a65f32ebeffec35cb1539
PatCode9 <45809622+PatCode9@users.noreply.github.com>
2023-02-16 16:41:15 +0800

    fix bug and added new conditions

--- components/shared/ManualStep.vue
+++ components/shared/ManualStep.vue
@@ -8,17 +8,37 @@
     <p v-for="info in infos" class="manual-info">
       <span v-if="info.includes('Active email address:')">
         <b>{{ info.substring(0, 21) }}</b>
-        {{ info.substring(21) }}
+        {{ info.substring(21, info.length) }}
       </span>
 
       <span v-else-if="info.includes('Aktibong email address:')">
-        <b>{{ info.substring(0, 24) }}</b>
-        {{ info.substring(24) }}
+        <b>{{ info.substring(0, 23) }}</b>
+        {{ info.substring(23) }}
       </span>
 
-      <span v-if="info.includes('Password:')">
-        <b>{{ info.substring(0, 10) }}</b>
-        {{ info.substring(10) }}
+      <span v-else-if="info.includes('Password:')">
+        <b>{{ info.substring(0, 9) }}</b>
+        {{ info.substring(9) }}
+      </span>
+
+      <span v-else-if="info.includes('Choose Sole Proprietorship:')">
+        <b>{{ info.substring(0, 27) }}</b>
+        {{ info.substring(27) }}
+      </span>
+
+      <span v-else-if="info.includes('Choose Partnership/Corporation:')">
+        <b>{{ info.substring(0, 31) }}</b>
+        {{ info.substring(31) }}
+      </span>
+
+      <span v-else-if="info.includes('Choose Community Association:')">
+        <b>{{ info.substring(0, 29) }}</b>
+        {{ info.substring(29) }}
+      </span>
+
+      <span v-else-if="info.includes('Choose Government:')">
+        <b>{{ info.substring(0, 18) }}</b>
+        {{ info.substring(18) }}
       </span>
 
       <span v-else>
